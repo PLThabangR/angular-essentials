ca087ffe234184398ebbeb6ce3e597f719c46b3e
Thabang Rakgoropo <rakgoropothabang@gmail.com>
2023-11-23 03:30:43 +0200

    Explicit creating formFroup

--- src/app/authors/authors.component.ts
+++ src/app/authors/authors.component.ts
@@ -1,19 +1,28 @@
 import { CoursesService } from './../services/courses.service';
 import { Component } from '@angular/core';
-
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-authors',
   templateUrl: './authors.component.html',
   styleUrls: ['./authors.component.css']
 })
 export class AuthorsComponent {
-credentials={
-  email:'',
-  comment:''
-}
+
+  name= new FormControl("",[Validators.required, Validators.minLength(4)])
+  email = new FormControl('', [Validators.required, Validators.email]);
+  comment = new FormControl('', [Validators.required, Validators.minLength(4)]);
+
+registerForm = new FormGroup(
+  {
+    name:this.name,
+    email:this.email,
+    comment:this.comment
+
+  }
+)
 
   login(){
-console.log(this.credentials);
+
 }
 
 
